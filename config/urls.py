f14273ba2dbff5e936791f4168b305bf8bb3f1d2
NufelRokniDev <nufel.rokni.dev@gmail.com>
2026-04-14 10:30:00 +0700

    feat: add favicons and /favicon.ico root redirect

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 from apps.pages.sitemaps import NewsSitemap, ProductSitemap, StaticViewSitemap
 
@@ -18,6 +18,7 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('favicon.ico', RedirectView.as_view(url='/static/img/favicon-48.png', permanent=True)),
 ]
 
 urlpatterns += [
